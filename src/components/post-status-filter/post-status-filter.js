3f76336e43c42c01ada66cb5f7242ceba8f20abc
Alymbek uulu Dastan <alymbek.uulu.d@gmail.com>
2022-01-20 15:28:41 +0600

    finished

--- src/components/post-status-filter/post-status-filter.js
+++ src/components/post-status-filter/post-status-filter.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, {Component} from "react";
 import "./post-status-filter.css";
 
-const PostStatusFilter = () => {
-    return (
-        <div className="btn-group">
-            <button/>
-            <button/>
-        </div>
-    )
-}
+export default class PostStatusFilter extends Component {
 
-export default PostStatusFilter;
+    constructor(props) {
+        super(props);
+
+        this.buttons = [
+            {name: "all", label: "Все"},
+            {name: "like", label: "Понравилось"}
+        ]
+    }
+
+    render() {
+        const {filter, onFilterSelect} = this.props;
+        const buttons = this.buttons.map(({name, label}) => {
+            const active = filter === name;
+            const clazz = active ? "btn-info" : "btn-outline-secondary";
+            return (
+                <button 
+                key={name} 
+                type="button" 
+                className={`btn ${clazz}`}
+                onClick={() => {onFilterSelect(name)}}>{label}</button>
+            )
+        })
+        return (
+            <div className="btn-group">
+                {buttons}
+            </div>
+        )
+    }
+    
+}
